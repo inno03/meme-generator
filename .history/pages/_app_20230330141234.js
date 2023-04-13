2c74336ad9c15777a62b21dacb958ce012ed884a
Innocent <rupiahinnocent66@gmail.com>
2023-04-13 21:33:27 +0300

    first commit

---
+++ .history/pages/_app_20230330141234.js
@@ -0,0 +1,7 @@
+import '@/styles/globals.css'
+import React from "react"
+import Header from "./components/Header"
+
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
